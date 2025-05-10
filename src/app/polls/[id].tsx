a6bfc3eae0249a823808edb5b076fea5192d65c9
Ashok Neupane <ashokabrother@gmail.com>
2025-05-10 13:07:40 +0545

    dynamic page made with options #1.06

--- src/app/polls/[id].tsx
+++ src/app/polls/[id].tsx
@@ -1,5 +1,7 @@
-import { useLocalSearchParams } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { View, Text, StyleSheet, Pressable, Button } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
 
 const poll = {
   question: "React Native vs Flutter ?",
@@ -7,19 +9,36 @@ const poll = {
 };
 
 export default function PollDetails() {
+  const [selected, setSelected] = useState("React Native FTW");
+  const vote = () => {
+    console.warn("Vote:", selected);
+  };
+
   const { id } = useLocalSearchParams<{ id: string }>();
 
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ title: "Choose Leader" }} />
+
       <Text style={styles.question}>{poll.question}</Text>
 
-      <View style={{gap:5}}>
+      <View style={{ gap: 5 }}>
         {poll.options.map((option) => (
-          <View key={option} style={styles.optionContainer}>
+          <Pressable
+            onPress={() => setSelected(option)}
+            key={option}
+            style={styles.optionContainer}
+          >
+            <Feather
+              name={option === selected ? "check-circle" : "circle"}
+              size={18}
+              color={option === selected ? "green" : "gray"}
+            />
             <Text>{option}</Text>
-          </View>
+          </Pressable>
         ))}
       </View>
+      <Button onPress={vote} title="Vote" />
     </View>
   );
 }
@@ -27,7 +46,7 @@ export default function PollDetails() {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-    gap:10,
+    gap: 10,
   },
   question: {
     fontSize: 20,
@@ -35,7 +54,10 @@ const styles = StyleSheet.create({
   },
   optionContainer: {
     backgroundColor: "white",
-    padding:10,
-    borderRadius:5,
+    padding: 10,
+    borderRadius: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
   },
 });
